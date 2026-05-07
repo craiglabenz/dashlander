dba582f7749a34dce254d2593e17693766b456d2
Craig Labenz <craig.labenz@gmail.com>
2026-05-07 16:21:46 -0700

    adds deployment

---
+++ site/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "Building Flutter app..."
+./build_flutter.sh
+
+echo "Building Jaspr site..."
+jaspr build
+
+echo "Deploying to Firebase Hosting..."
+firebase deploy --only hosting
+
+echo "Deployment complete!"
